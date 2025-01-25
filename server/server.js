deed1989204b81742b55ef6bee2729ec16d0d6ae
Sneha-Mshr <snehamishra132003@gmail.com>
2025-01-25 11:54:14 +0530

    :tada: Finally Deploying

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://codeholic-ivory.vercel.app",
         credentials: true,
     })
 )
